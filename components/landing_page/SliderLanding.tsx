a1845354f21c16b38b8d062eeb8d5edc3f1cbe62
SamyaRoy999 <samyaroy999@gmail.com>
2025-07-26 10:21:15 +0600

    lending page Carousel bug fixed

--- components/landing_page/SliderLanding.tsx
+++ components/landing_page/SliderLanding.tsx
@@ -1,6 +1,7 @@
 import { IconNextLeft, IconNextRight } from "@/icons/Icon";
 import tw from "@/lib/tailwind";
 import { useBannerQuery } from "@/redux/apiSlices/Home/homeApiSlices";
+import { _Width } from "@/utils/utils";
 import { Image } from "expo-image";
 import * as React from "react";
 import { TouchableOpacity, View } from "react-native";
@@ -25,27 +26,31 @@ const SliderLanding = () => {
 
 
   return (
-    <View style={tw`w-full relative`}>
+    <View style={tw`w-full relative `}>
       <Carousel
         ref={carouselRef}
         autoPlayInterval={3000}
         data={banner?.data?.data} // backend images used here
         height={230}
-        width={380}
+        width={_Width}
+       
         loop
         pagingEnabled
         snapEnabled
         mode="parallax"
         modeConfig={{
-          parallaxScrollingScale: 0.9,
+          parallaxScrollingScale: 0.95,
           parallaxScrollingOffset: 50,
         }}
         onProgressChange={progress}
         renderItem={({ item }: any) => (
-          <View style={{ flex: 1, borderRadius: 20 }}>
+          <View style={tw`flex-1 rounded-3xl items-center justify-center`}>
             <Image
               source={{ uri: item.image }}
-              style={tw`h-[200px] w-[380px] rounded-2xl`}
+              style={[tw`h-[200px]  rounded-2xl`,{
+                width: _Width * 0.92,
+
+              }]}
               contentFit="cover"
             />
           </View>
@@ -55,7 +60,7 @@ const SliderLanding = () => {
 
       {/* Navigation Buttons */}
       <View
-        style={tw`flex-row w-full justify-between px-9 absolute top-[34%] right-4`}
+        style={tw`flex-row w-full justify-between items-center px-9 absolute top-[40%] `}
       >
         <TouchableOpacity onPress={() => carouselRef.current?.prev()}>
           <SvgXml xml={IconNextLeft} />
